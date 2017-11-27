bc078b26abc06597b081361552c03ec69fd9af57
Edward <gonelove.guo@gmail.com>
2017-11-27 23:44:31 +0800

    vue typescript 之路

--- src/base/scroll.vue
+++ src/base/scroll.vue
@@ -8,12 +8,33 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 
-@Component
-export default class ItemClass extends Vue {
+// @Component 修饰符注明了此类为一个 Vue 组件
+@Component({
+  props: {
+    probeType: {
+      type: Number,
+      default: 1
+    },
+    click: {
+      type: Boolean,
+      default: true
+    },
+    data: {
+      type: Array,
+      default: null
+    },
+    listenScroll: {
+      type: Boolean
+    }
+  }
+})
+export default class Scroll extends Vue {
+  probeType: Number;
+  data: object
   msg: string = 'Hello!'
+  helloMsg: string = 'hello' + this.probeType;
   mounted () {
   }
-
   get computedMsg () {
     return 'computed' + this.msg;
   }
